0357ede3d31e45d316c63124e61a723a96de957a
ssdd33 <beeblu.0@gmail.com>
2022-12-21 20:58:45 +0900

    tabBar & tabView : initState에서 TabController 초기화 -> 오류 발생 (error.md 참고)

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:doit_flutter_practice/sub/firstPage.dart';
+import 'package:doit_flutter_practice/sub/secondPage.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -13,103 +15,70 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _MyHomePageState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin {
+  TabController? controller;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  @override
+  void initState() {
+    super.initState();
+    controller = TabController(length: 2, vsync: this);
+    /*
+    * length: 몇 개의 탭을 만들지 지정
+    * vsync: 탭이 이동했을 때 호출되는 콜백 함수를 어디서 처리할지 지정
+    * state클래스에 with 키워드로 SingleTickerProviderStateMixin클래스를 지정하면 vsync : this 오류가 해결됨
+    * 클래스를 추가 상속함으로써 탭을 눌렀을 때 state클래스에서 애니메이션 동작을 처리할 수 있게 함*/
+  }
+
+  @override
+  void dispose() {
+    /*
+    * 스테이트풀이 마지막에 호출하는 함수
+    * 탭 컨트롤러는 애니메이션을 이용하므로 dispose()함수를 호출해 주어야 메모리 누수를 막을 수 있다.*/
+    controller!.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('TabBar example'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+      body: TabBarView(
+        controller: controller,
+        children: [FirstApp(), SecondApp()],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      bottomNavigationBar: TabBar(
+        tabs: <Tab>[
+          Tab(
+            icon: Icon(
+              Icons.looks_one,
+              color: Colors.blue,
+            ),
+            text: '목록',
+          ),
+          Tab(
+            icon: Icon(
+              Icons.looks_two,
+              color: Colors.blue,
+            ),
+            text: '추가하기',
+          ),
+        ],
+      ),
     );
   }
 }
---
+++ lib/sub/secondPage.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class SecondApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text('second page'),
+      ),
+    );
+  }
+}
