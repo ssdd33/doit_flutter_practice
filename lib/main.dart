d5d1832f8019968630495c13a4704faec799ec24
ssdd33 <beeblu.0@gmail.com>
2022-12-29 18:20:49 +0900

    JSON 데이터 파싱, listView로 데이터 목록 화면에 표시하기

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-
+import 'dart:convert';
 
 void main() {
   runApp(const MyApp());
@@ -27,6 +27,13 @@ class HttpApp extends StatefulWidget {
 
 class _HttpAppState extends State<HttpApp> {
   String result = '';
+  List data =  List.empty(growable:true);
+
+  // @override
+  // void initState(){
+  //   super.initState();
+  //   data = List.empty(growable:true);
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -34,19 +41,50 @@ class _HttpAppState extends State<HttpApp> {
       appBar:AppBar(title:Text('Http Example')),
       body:Container(
         child: Center(
-          child: Text('$result'),
+          child:data.length==0?Text('데이터가 없습니다.',style:TextStyle(fontSize:20),textAlign:TextAlign.center):ListView.builder(
+            itemBuilder: (context, index){
+              return Card(
+                child: Container(
+                  child: Column(
+                    children: [
+                      Text(data[index]['title'].toString()),
+                      Text(data[index]['authors'].toString()),
+                      Text(data[index]['sale_price'].toString()),
+                      Text(data[index]['status'].toString()),
+                      Image.network(
+                        data[index]['thumbnail'],
+                        height:100,
+                        width:100,
+                        fit:BoxFit.contain,
+                      )
+                    ],
+                  ),
+                ),
+              );
+            },
+            itemCount: data.length,
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: ()async{
-          var url = 'http://google.com';
-          var response = await http.get(Uri.parse(url));
-          setState(() {
-            result = response.body;
-          });
+        onPressed: (){
+          getJSONData();
         },
         child:Icon(Icons.file_download)
       ),
     );
+  }
+
+  Future<String> getJSONData() async{
+    var url = 'http://dapi.kakao.com/v3/search/book?target=title&query=doit';
+    var response  =await http.get(Uri.parse(url),
+    headers: {"Authorization":"KakaoAK 4e980e571507894e8f5392267de73319"});
+   setState(() {
+     var dataConvertedToJSON = json.decode(response.body);
+     List result = dataConvertedToJSON['documents'];
+     data.addAll(result);
+   });
+
+   return response.body;
   }
 }
