8436c22327fb178aa5cc563ad4f0b64d8675108d
ssdd33 <beeblu.0@gmail.com>
2022-12-29 20:39:19 +0900

    Navigator 반환값을 이용해 할일목록 추가기능 구현

--- lib/secondDetail.dart
+++ lib/secondDetail.dart
@@ -1,19 +1,25 @@
 import 'package:flutter/material.dart';
 
 class SecondDetail extends StatelessWidget {
-  const SecondDetail({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    TextEditingController controller = new TextEditingController();
+
     return Scaffold(
       appBar: AppBar(title:Text('Second Page'),),
       body:Container(
         child:Center(
-          child: ElevatedButton(
-            onPressed:(){
-              Navigator.of(context).pushReplacementNamed('/third');
-            },
-            child: Text('돌아가기'),
+          child: Column(
+            children: [
+              TextField(
+                controller:controller,
+                keyboardType: TextInputType.text,
+              ),
+              ElevatedButton(onPressed: (){
+                Navigator.of(context).pop(controller.value.text);
+              }, child: Text('저장하기'))
+            ],
           ),
         ),
       )
